558c7193021a51acce4976b5f2735e3ad38e6893
Vinícius <vinicius.s.lisboa@gmail.com>
2021-07-03 10:20:16 -0300

    .finance

--- scripts.js
+++ scripts.js
@@ -84,7 +84,7 @@ const DOM = {
             <td class="${CSSclass}">${amount}</td>
             <td class="date">${transaction.date}</td>
             <td>
-                <img onclick="Transaction.remove(${index})"src="./assets/minus.svg" alt="remover transação">
+                <img class="imgHtml"onclick="Transaction.remove(${index})"src="./assets/minus.svg" alt="remover transação">
             </td>
         `
 
